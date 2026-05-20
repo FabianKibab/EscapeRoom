81970bb13acbd82cfef324ee4a766dab6f811dc9
Fabian <167702844+FabianKibab@users.noreply.github.com>
2026-05-20 14:56:12 +0200

    First Slot Machine Update
    You can spin the slots using poke interaction

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float deltaX = 20f;
+
+    private float currentX = 90f;
+
+    private const float fixedY = 90f;
+    private const float fixedZ = 90f;
+
+    void Start()
+    {
+        ApplyRotation();
+    }
+
+    public void RotateX()
+    {
+        currentX += deltaX;
+
+        ApplyRotation();
+
+        Debug.Log("X = " + currentX);
+    }
+
+    private void ApplyRotation()
+    {
+        transform.localRotation = Quaternion.Euler(
+            currentX,
+            fixedY,
+            fixedZ
+        );
+    }
+}
